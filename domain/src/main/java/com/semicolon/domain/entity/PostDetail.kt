7daec2aeb9e325341161c9f649d119617ba9f506
shw0471 <shw0471@naver.com>
2021-08-27 22:39:16 +0900

    add PostDetail data class

---
+++ domain/src/main/java/com/semicolon/domain/entity/PostDetail.kt
@@ -0,0 +1,44 @@
+package com.semicolon.domain.entity
+
+data class PostDetail(
+
+    val nickname: String,
+
+    val rating: String,
+
+    val post: PostInfo,
+
+    val pet: PetInfo
+){
+    data class PostInfo(
+
+        val title: String,
+
+        val protectionStartDate: String,
+
+        val protectionEndDate: String,
+
+        val applicationEndDate: String,
+
+        val description: String,
+
+        val contactInfo: String,
+
+        val createdAt: String,
+
+        val isApplicationEnd: Boolean,
+
+        val isUpdated: Boolean
+    )
+
+    data class PetInfo(
+
+        val petName: String,
+
+        val petSpecies: String,
+
+        val petSex: String,
+
+        val filePaths: List<String>
+    )
+}
