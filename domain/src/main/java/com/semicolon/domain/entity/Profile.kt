ede7fb38451c1ce3684dfe8aad815ad28a74e77c
shw0471 <shw0471@naver.com>
2021-08-27 22:08:27 +0900

    add Profile data class

---
+++ domain/src/main/java/com/semicolon/domain/entity/Profile.kt
@@ -0,0 +1,22 @@
+package com.semicolon.domain.entity
+
+data class Profile(
+
+    val nickname: String,
+
+    val avgGrade: Double,
+
+    val rating: String,
+
+    val age: Int,
+
+    val sex: String,
+
+    val experience: String,
+
+    val locationConfirm: Boolean,
+
+    val experienceRaisingPet: String,
+
+    val administrationDivision: String
+)
