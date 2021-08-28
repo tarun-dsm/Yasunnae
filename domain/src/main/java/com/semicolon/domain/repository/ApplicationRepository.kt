3d5d032a8c8ad84c92876596ac11e6962953e1cd
shw0471 <shw0471@naver.com>
2021-08-28 21:30:27 +0900

    update ApplicationRepository interface

---
+++ domain/src/main/java/com/semicolon/domain/repository/ApplicationRepository.kt
@@ -0,0 +1,16 @@
+package com.semicolon.domain.repository
+
+import com.semicolon.domain.entity.ApplicationEntity
+import io.reactivex.Completable
+import io.reactivex.Single
+
+interface ApplicationRepository {
+
+    fun sendApplication(id: Int): Completable
+
+    fun cancelApplication(id: Int): Completable
+
+    fun acceptApplication(id: Int): Completable
+
+    fun getMyApplicationList(id: Int): Single<List<ApplicationEntity>>
+}
