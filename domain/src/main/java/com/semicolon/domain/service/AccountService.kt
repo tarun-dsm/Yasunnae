521a1cd0b8799f6d8c4c7f9f3a94d7ef66d87c4f
shw0471 <shw0471@naver.com>
2021-08-29 01:06:01 +0900

    update AccountService interface

---
+++ domain/src/main/java/com/semicolon/domain/service/AccountService.kt
@@ -0,0 +1,16 @@
+package com.semicolon.domain.service
+
+import com.semicolon.domain.base.Resource
+import com.semicolon.domain.param.RegisterAccountParam
+import io.reactivex.Single
+
+interface AccountService {
+
+    fun registerAccount(registerAccountParam: RegisterAccountParam): Single<Resource<Unit>>
+
+    fun checkEmailDuplication(email: String): Single<Resource<Unit>>
+
+    fun checkNicknameDuplication(nickname: String): Single<Resource<Unit>>
+
+    fun saveCoordinate(longitude: Double, latitude: Double): Single<Resource<Unit>>
+}
