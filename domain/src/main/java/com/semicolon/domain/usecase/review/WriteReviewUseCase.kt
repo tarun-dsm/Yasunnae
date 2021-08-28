a5399192056fd406a4a722cb1906429ccd5ecd0b
shw0471 <shw0471@naver.com>
2021-08-29 03:09:58 +0900

    update WriteReviewUseCase class

---
+++ domain/src/main/java/com/semicolon/domain/usecase/review/WriteReviewUseCase.kt
@@ -0,0 +1,15 @@
+package com.semicolon.domain.usecase.review
+
+import com.semicolon.domain.base.Resource
+import com.semicolon.domain.base.UseCase
+import com.semicolon.domain.param.ReviewParam
+import com.semicolon.domain.service.ReviewService
+import io.reactivex.Single
+
+class WriteReviewUseCase(
+    private val reviewService: ReviewService
+) : UseCase<ReviewParam, Resource<Unit>>() {
+
+    override fun interact(data: ReviewParam?): Single<Resource<Unit>> =
+        reviewService.writeReview(data!!)
+}
