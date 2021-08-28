735278bde18aa54c7dbd1a6e8cbf9c6ce5cfa55e
shw0471 <shw0471@naver.com>
2021-08-29 01:09:42 +0900

    update AccountServiceImpl class

---
+++ domain/src/main/java/com/semicolon/domain/service/AccountServiceImpl.kt
@@ -0,0 +1,26 @@
+package com.semicolon.domain.service
+
+import com.semicolon.domain.base.ErrorHandler
+import com.semicolon.domain.base.Resource
+import com.semicolon.domain.base.toSingleResource
+import com.semicolon.domain.param.RegisterAccountParam
+import com.semicolon.domain.repository.AccountRepository
+import io.reactivex.Single
+
+class AccountServiceImpl(
+    private val accountRepository: AccountRepository,
+    private val errorHandler: ErrorHandler
+) : AccountService {
+
+    override fun registerAccount(registerAccountParam: RegisterAccountParam): Single<Resource<Unit>> =
+        accountRepository.registerAccount(registerAccountParam).toSingleResource(errorHandler)
+
+    override fun checkEmailDuplication(email: String): Single<Resource<Unit>> =
+        accountRepository.checkEmailDuplication(email).toSingleResource(errorHandler)
+
+    override fun checkNicknameDuplication(nickname: String): Single<Resource<Unit>> =
+        accountRepository.checkNicknameDuplication(nickname).toSingleResource(errorHandler)
+
+    override fun saveCoordinate(longitude: Double, latitude: Double): Single<Resource<Unit>> =
+        accountRepository.saveCoordinate(longitude, latitude).toSingleResource(errorHandler)
+}
