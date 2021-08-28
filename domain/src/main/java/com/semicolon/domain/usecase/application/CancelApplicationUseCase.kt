461fce9d392cbb8ae6fa439a8aa9c9e989b1b9e1
shw0471 <shw0471@naver.com>
2021-08-29 02:31:26 +0900

    update CancelApplicationUseCase class

---
+++ domain/src/main/java/com/semicolon/domain/usecase/application/CancelApplicationUseCase.kt
@@ -0,0 +1,13 @@
+package com.semicolon.domain.usecase.application
+
+import com.semicolon.domain.base.Resource
+import com.semicolon.domain.base.UseCase
+import com.semicolon.domain.service.ApplicationService
+import io.reactivex.Single
+
+class CancelApplicationUseCase(
+    private val applicationService: ApplicationService
+) : UseCase<Int, Resource<Unit>>() {
+    override fun interact(data: Int): Single<Resource<Unit>> =
+        applicationService.cancelApplication(data)
+}
