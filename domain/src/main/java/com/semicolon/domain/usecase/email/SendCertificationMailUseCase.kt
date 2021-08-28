7ed0b1472922ab1e0f78584f837097199249fab1
shw0471 <shw0471@naver.com>
2021-08-29 02:42:16 +0900

    update sendCertificationMailUseCase class

---
+++ domain/src/main/java/com/semicolon/domain/usecase/email/SendCertificationMailUseCase.kt
@@ -0,0 +1,14 @@
+package com.semicolon.domain.usecase.email
+
+import com.semicolon.domain.base.Resource
+import com.semicolon.domain.base.UseCase
+import com.semicolon.domain.service.EmailService
+import io.reactivex.Single
+
+class SendCertificationMailUseCase(
+    private val emailService: EmailService
+) : UseCase<String, Resource<Unit>>() {
+
+    override fun interact(data: String): Single<Resource<Unit>> =
+        emailService.sendCertificationMail(data)
+}
