bd9ac228fe02eb87c49ec22888261846da8b07cb
shw0471 <shw0471@naver.com>
2021-08-29 02:51:29 +0900

    update getPostDetailUseCase class

---
+++ domain/src/main/java/com/semicolon/domain/usecase/post/GetPostDetailUseCase.kt
@@ -0,0 +1,15 @@
+package com.semicolon.domain.usecase.post
+
+import com.semicolon.domain.base.Resource
+import com.semicolon.domain.base.UseCase
+import com.semicolon.domain.entity.PostDetailEntity
+import com.semicolon.domain.service.PostService
+import io.reactivex.Single
+
+class GetPostDetailUseCase(
+    private val postService: PostService
+) : UseCase<Int, Resource<PostDetailEntity>>() {
+
+    override fun interact(data: Int): Single<Resource<PostDetailEntity>> =
+        postService.getPostDetail(data)
+}
