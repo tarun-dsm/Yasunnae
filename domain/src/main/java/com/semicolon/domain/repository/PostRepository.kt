f22d69d4124b68c3d685ba6f3d9336dd70f8bc61
shw0471 <shw0471@naver.com>
2021-08-28 20:57:10 +0900

    update PostRepository interface

---
+++ domain/src/main/java/com/semicolon/domain/repository/PostRepository.kt
@@ -0,0 +1,23 @@
+package com.semicolon.domain.repository
+
+import com.semicolon.domain.entity.PostDetailEntity
+import com.semicolon.domain.entity.PostEntity
+import com.semicolon.domain.param.PostParam
+import io.reactivex.Completable
+import io.reactivex.Single
+import java.io.File
+
+interface PostRepository {
+
+    fun sendPostImage(id: Int, image: File): Completable
+
+    fun writePost(postParam: PostParam): Single<Int>
+
+    fun getPostList(): Single<List<PostEntity>>
+
+    fun fixPost(id:Int, postParam: PostParam): Single<Int>
+
+    fun deletePost(id: Int): Completable
+
+    fun getPostDetail(id: Int): Single<PostDetailEntity>
+}
