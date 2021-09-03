7ca2b361ccae8e41ddb9187880fdc45fdde84fce
shw0471 <shw0471@naver.com>
2021-09-04 01:41:15 +0900

    add ProfileResponse data class

---
+++ data/src/main/java/com/semicolon/data/remote/response/ProfileResponse.kt
@@ -0,0 +1,46 @@
+package com.semicolon.data.remote.response
+
+import com.google.gson.annotations.SerializedName
+import com.semicolon.domain.entity.ProfileEntity
+
+data class ProfileResponse(
+
+    @SerializedName("nickname")
+    val nickname: String,
+
+    @SerializedName("avg_grade")
+    val avgGrade: Double,
+
+    @SerializedName("rating")
+    val rating: String,
+
+    @SerializedName("age")
+    val age: Int,
+
+    @SerializedName("sex")
+    val sex: String,
+
+    @SerializedName("experience")
+    val experience: String,
+
+    @SerializedName("location_confirm")
+    val locationConfirm: Boolean,
+
+    @SerializedName("experience_raising_pet")
+    val experienceRaisingPet: String,
+
+    @SerializedName("administration_division")
+    val administrationDivision: String
+)
+
+fun ProfileResponse.toEntity() = ProfileEntity(
+    nickname = this.nickname,
+    avgGrade = this.avgGrade,
+    rating = this.rating,
+    age = this.age,
+    sex = this.sex,
+    experience = this.experience,
+    locationConfirm = this.locationConfirm,
+    experienceRaisingPet = this.experienceRaisingPet,
+    administrationDivision = this.administrationDivision
+)
