e7120ca2df9e73626c51983aa3229fc5a064ec71
shw0471 <shw0471@naver.com>
2021-09-04 22:50:49 +0900

    update AuthRepositoryImpl class

---
+++ data/src/main/java/com/semicolon/data/repository/AuthRepositoryImpl.kt
@@ -0,0 +1,17 @@
+package com.semicolon.data.repository
+
+import com.semicolon.data.datasource.AuthDataSource
+import com.semicolon.domain.param.LoginParam
+import com.semicolon.domain.repository.AuthRepository
+import io.reactivex.Completable
+
+class AuthRepositoryImpl(
+    private val authDataSource: AuthDataSource
+) : AuthRepository {
+
+    override fun login(loginParam: LoginParam): Completable =
+        authDataSource.login(loginParam)
+
+    override fun tokenRefresh(): Completable =
+        authDataSource.tokenRefresh()
+}
