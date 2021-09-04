470b7b16b213942867e4417dd2055f84ed137b51
shw0471 <shw0471@naver.com>
2021-09-04 22:21:53 +0900

    update EmailDataSourceImpl class

---
+++ data/src/main/java/com/semicolon/data/datasource/EmailDataSourceImpl.kt
@@ -0,0 +1,21 @@
+package com.semicolon.data.datasource
+
+import com.semicolon.data.local.TokenStorage
+import com.semicolon.data.remote.api.EmailApi
+import com.semicolon.data.remote.request.SendCertificationEmailRequest
+import com.semicolon.domain.param.EmailCertificationParam
+import io.reactivex.Completable
+
+class EmailDataSourceImpl(
+    private val emailApi: EmailApi
+) : EmailDataSource {
+
+    override fun checkCertificationNumber(emailCertificationParam: EmailCertificationParam): Completable =
+        emailApi.checkCertificationNumber(
+            emailCertificationParam.email,
+            emailCertificationParam.number
+        )
+
+    override fun sendCertificationEmail(email: String): Completable =
+        emailApi.sendCertificationEmail(SendCertificationEmailRequest(email))
+}
