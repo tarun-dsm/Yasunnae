206a263542a45c319a7f39d4019d71ee691ad48c
shw0471 <shw0471@naver.com>
2021-09-14 19:30:36 +0900

    make Coordinate Activity

---
+++ app/src/main/java/com/semicolon/yasunnae/ui/coordinate/CoordinateActivity.kt
@@ -0,0 +1,12 @@
+package com.semicolon.yasunnae.ui.coordinate
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.semicolon.yasunnae.R
+
+class CoordinateActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_coordinate)
+    }
+}
