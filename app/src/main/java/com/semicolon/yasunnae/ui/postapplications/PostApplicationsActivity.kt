98b8dfc95fda6c0f0ac5e35fb73cdadb2caecd04
shw0471 <shw0471@naver.com>
2021-09-27 09:24:17 +0900

    add PostApplicationsActivity

---
+++ app/src/main/java/com/semicolon/yasunnae/ui/postapplications/PostApplicationsActivity.kt
@@ -0,0 +1,12 @@
+package com.semicolon.yasunnae.ui.postapplications
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.semicolon.yasunnae.R
+
+class PostApplicationsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_post_applications)
+    }
+}
