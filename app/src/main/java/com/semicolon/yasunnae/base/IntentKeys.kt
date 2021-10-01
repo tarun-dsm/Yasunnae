a780362d2a08bf58e213c78a48e7926f52e13474
shw0471 <shw0471@naver.com>
2021-10-01 11:40:10 +0900

    update IntentKeys object

---
+++ app/src/main/java/com/semicolon/yasunnae/base/IntentKeys.kt
@@ -0,0 +1,7 @@
+package com.semicolon.yasunnae.base
+
+object IntentKeys {
+    const val KEY_POST_ID = "KeyPostId"
+    const val KEY_START_DATE = "KeyStartDate"
+    const val KEY_END_DATE = "KeyEndDate"
+}
