61e1e77202753361d011f689a1ddb143d3b82636
shw0471 <shw0471@naver.com>
2021-10-03 06:21:18 +0900

    update BaseFragment abstract class

---
+++ app/src/main/java/com/semicolon/yasunnae/base/BaseFragment.kt
@@ -0,0 +1,37 @@
+package com.semicolon.yasunnae.base
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
+import androidx.databinding.DataBindingUtil
+import androidx.databinding.ViewDataBinding
+import androidx.fragment.app.Fragment
+
+abstract class BaseFragment<T : ViewDataBinding> : Fragment() {
+
+    lateinit var binding: T
+
+    abstract val layoutResId: Int
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = DataBindingUtil.inflate(inflater, layoutResId, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.lifecycleOwner = this
+        init()
+    }
+
+    abstract fun init()
+
+    protected fun makeToast(msg: String) =
+        Toast.makeText(context, msg, Toast.LENGTH_SHORT).show()
+}
