91deb3445dfc982c9efe5b4d4a40c8c2e2fa09ac
shw0471 <shw0471@naver.com>
2021-10-03 03:34:38 +0900

    update MainActivity class

--- app/src/main/java/com/semicolon/yasunnae/ui/main/MainActivity.kt
+++ app/src/main/java/com/semicolon/yasunnae/ui/main/MainActivity.kt
@@ -1,12 +1,40 @@
 package com.semicolon.yasunnae.ui.main
 
-import androidx.appcompat.app.AppCompatActivity
-import android.os.Bundle
+import androidx.fragment.app.Fragment
 import com.semicolon.yasunnae.R
+import com.semicolon.yasunnae.base.BaseActivity
+import com.semicolon.yasunnae.databinding.ActivityMainBinding
+import com.semicolon.yasunnae.ui.postlist.PostListFragment
 
-class MainActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+class MainActivity : BaseActivity<ActivityMainBinding>() {
+
+    override val layoutResId: Int
+        get() = R.layout.activity_main
+
+    override fun init() {
+        binding.bnvMain.setOnItemSelectedListener {
+            when (it.itemId) {
+                R.id.menu_home -> setFragment(PostListFragment())
+                R.id.menu_write_post -> startWritePostActivity()
+                R.id.menu_my_applications -> TODO("내 신청목록 Fragment 실행")
+                R.id.menu_my_profile -> TODO("마이 프로필 Fragment 로 실행")
+            }
+            return@setOnItemSelectedListener true
+        }
+        binding.bnvMain.selectedItemId = R.id.menu_home
     }
+
+    override fun observe() {}
+
+    private fun setFragment(fragment: Fragment) {
+        supportFragmentManager.beginTransaction().replace(
+            binding.flContainerMail.id, fragment
+        ).commit()
+    }
+
+    private fun startWritePostActivity() {
+        TODO("게시글 작성 Activity 실행")
+    }
+
 }
