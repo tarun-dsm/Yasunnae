336f1305331c6daea399f5b55d657a8da3837ac9
shw0471 <shw0471@naver.com>
2021-10-03 07:26:29 +0900

    add register account activity

---
+++ app/src/main/java/com/semicolon/yasunnae/ui/registeraccount/RegisterAccountActivity.kt
@@ -0,0 +1,12 @@
+package com.semicolon.yasunnae.ui.registeraccount
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.semicolon.yasunnae.R
+
+class RegisterAccountActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register_account)
+    }
+}
