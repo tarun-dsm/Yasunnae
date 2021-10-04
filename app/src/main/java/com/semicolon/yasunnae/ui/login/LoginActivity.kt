b019750f054d85ef8bfc2b799db78e8e88fd075c
권민정 <kmjdec1203@gmail.com>
2021-10-04 16:44:11 +0900

    update LoginActivity

---
+++ app/src/main/java/com/semicolon/yasunnae/ui/login/LoginActivity.kt
@@ -0,0 +1,13 @@
+package com.semicolon.yasunnae.ui.login
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.semicolon.yasunnae.R
+
+class LoginActivity: AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
