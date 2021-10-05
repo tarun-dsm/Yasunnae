1a40296855702bc57ab25b5160e7c4efec629d02
shw0471 <shw0471@naver.com>
2021-10-05 09:15:34 +0900

    add WritePostActivity

---
+++ app/src/main/java/com/semicolon/yasunnae/ui/writepost/WritePostActivity.kt
@@ -0,0 +1,12 @@
+package com.semicolon.yasunnae.ui.writepost
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.semicolon.yasunnae.R
+
+class WritePostActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_write_post)
+    }
+}
