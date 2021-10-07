605a968bc216a3bd2b07b76cd7fc661f014c00c4
shw0471 <shw0471@naver.com>
2021-10-07 20:05:33 +0900

    update AskDialog class

---
+++ app/src/main/java/com/semicolon/yasunnae/dialog/AskDialog.kt
@@ -0,0 +1,41 @@
+package com.semicolon.yasunnae.dialog
+
+import android.app.Dialog
+import android.content.Context
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
+import android.view.LayoutInflater
+import android.view.Window
+import androidx.databinding.DataBindingUtil
+import com.semicolon.yasunnae.R
+import com.semicolon.yasunnae.databinding.DialogAskBinding
+
+class AskDialog(
+    private val context: Context,
+    private val message: String,
+    private val onYesClick: () -> Unit = {},
+    private val onNoClick: () -> Unit = {}
+) {
+
+    fun callDialog() {
+        val dialog = Dialog(context)
+        dialog.requestWindowFeature(Window.FEATURE_NO_TITLE)
+        dialog.window!!.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
+
+        val binding: DialogAskBinding = DataBindingUtil.inflate(
+            LayoutInflater.from(context),
+            R.layout.dialog_ask,
+            null,
+            false
+        )
+        binding.tvAsk.text = message
+        binding.btnYesAsk.setOnClickListener {
+            onYesClick()
+            dialog.dismiss()
+        }
+        binding.btnNoAsk.setOnClickListener {
+            onNoClick()
+            dialog.dismiss()
+        }
+    }
+}
