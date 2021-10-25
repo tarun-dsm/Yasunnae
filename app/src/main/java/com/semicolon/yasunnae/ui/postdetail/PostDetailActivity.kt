d6391ef5386d67af905a0c514ef6642702a6f2a7
shw0471 <shw0471@naver.com>
2021-10-25 10:50:09 +0900

    add post detail activity

---
+++ app/src/main/java/com/semicolon/yasunnae/ui/postdetail/PostDetailActivity.kt
@@ -0,0 +1,12 @@
+package com.semicolon.yasunnae.ui.postdetail
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.semicolon.yasunnae.R
+
+class PostDetailActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_post_detail)
+    }
+}
