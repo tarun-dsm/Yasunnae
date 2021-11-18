7f1ed65d18f4da9af4dbf66e1015aff7573aad8c
권민정 <kmjdec1203@gmail.com>
2021-11-19 01:51:22 +0900

    fix LoginViewModel class
    Event 수정

--- app/src/main/java/com/semicolon/yasunnae/ui/login/LoginViewModel.kt
+++ app/src/main/java/com/semicolon/yasunnae/ui/login/LoginViewModel.kt
@@ -1,11 +1,9 @@
 package com.semicolon.yasunnae.ui.login
-import androidx.lifecycle.MutableLiveData
 import com.semicolon.domain.base.Error
 import com.semicolon.domain.base.ResourceStatus
 import com.semicolon.domain.base.Resource
 import com.semicolon.domain.param.LoginParam
 import com.semicolon.domain.usecase.auth.LoginUseCase
-import com.semicolon.domain.usecase.auth.TokenRefreshUseCase
 import com.semicolon.yasunnae.base.BaseViewModel
 import com.semicolon.yasunnae.base.SingleLiveEvent
 import dagger.hilt.android.lifecycle.HiltViewModel
@@ -15,30 +13,27 @@ import javax.inject.Inject
 @HiltViewModel
 class LoginViewModel @Inject constructor(
     private val loginUseCase: LoginUseCase,
-    private val tokenRefreshUseCase: TokenRefreshUseCase
 ) : BaseViewModel() {
 
-    val loginLiveData = MutableLiveData<Unit>()
-    val wrongConditionEvent = SingleLiveEvent<Unit>()
-    val nonExistentEmailEvent = SingleLiveEvent<Unit>()
+    val badRequestEvent = SingleLiveEvent<Unit>()
     val unknownErrorEvent = SingleLiveEvent<Unit>()
+    val successEvent = SingleLiveEvent<Unit>()
 
     fun login(login: LoginParam) {
         val result = loginUseCase.interact(login)
         val observer = object : DisposableSingleObserver<Resource<Unit>>() {
             override fun onSuccess(t: Resource<Unit>) {
                 when (t.status) {
-                    ResourceStatus.SUCCESS -> loginLiveData.value = t.data!!
+                    ResourceStatus.SUCCESS -> successEvent.call()
+
                     ResourceStatus.ERROR -> when (t.message) {
                         Error.BAD_REQUEST -> {
-                            wrongConditionEvent.call()
+                            badRequestEvent.call()
                         }
-                        Error.NOT_FOUND -> nonExistentEmailEvent.call()
                         else -> unknownErrorEvent.call()
                     }
                 }
             }
-
             override fun onError(e: Throwable) {
                 unknownErrorEvent.call()
             }
