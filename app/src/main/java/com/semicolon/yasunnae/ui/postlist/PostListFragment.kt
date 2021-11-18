2fd92baa2bff09ec58758c255e2656793bdb5dc1
shw0471 <shw0471@naver.com>
2021-11-18 10:41:32 +0900

    fix PostListFragment class

--- app/src/main/java/com/semicolon/yasunnae/ui/postlist/PostListFragment.kt
+++ app/src/main/java/com/semicolon/yasunnae/ui/postlist/PostListFragment.kt
@@ -7,10 +7,14 @@ import com.semicolon.domain.enum.AnimalType
 import com.semicolon.yasunnae.R
 import com.semicolon.yasunnae.adapter.PostListAdapter
 import com.semicolon.yasunnae.base.BaseFragment
+import com.semicolon.yasunnae.base.IntentKeys.KEY_POST_ID
 import com.semicolon.yasunnae.databinding.FragmentPostListBinding
 import com.semicolon.yasunnae.ui.coordinate.CoordinateActivity
 import com.semicolon.yasunnae.dialog.NeedToVerifyLocationDialog
+import com.semicolon.yasunnae.ui.postdetail.PostDetailActivity
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class PostListFragment : BaseFragment<FragmentPostListBinding>() {
 
     override val layoutResId: Int
@@ -19,20 +23,21 @@ class PostListFragment : BaseFragment<FragmentPostListBinding>() {
     private val postListViewModel: PostListViewModel by viewModels()
     private val postListAdapter = PostListAdapter(object : PostListAdapter.OnItemClickListener {
         override fun onItemClick(postId: Int) {
-            TODO("게시물 상세 페이지로 이동")
+            val intent = Intent(context, PostDetailActivity::class.java)
+            intent.putExtra(KEY_POST_ID, postId)
+            startActivity(intent)
         }
     })
 
     override fun init() {
+        binding.viewModel = postListViewModel
         binding.rvPostList.adapter = postListAdapter
         binding.btnGoVerifyLocation.setOnClickListener {
             startVerifyLocationActivity()
         }
-        binding.rgAnimalCategories.setOnCheckedChangeListener { group, checkedId ->
-            if (checkedId == group.checkedRadioButtonId) {
-                group.clearCheck()
-                postListAdapter.resetCategory()
-            } else when (checkedId) {
+        binding.rgAnimalCategories.setOnCheckedChangeListener { _, checkedId ->
+            when (checkedId) {
+                R.id.rb_all -> allCategory()
                 R.id.rb_mammal -> setCategory(AnimalType.MAMMAL)
                 R.id.rb_bird -> setCategory(AnimalType.BIRD)
                 R.id.rb_reptiles -> setCategory(AnimalType.REPTILES)
@@ -42,18 +47,25 @@ class PostListFragment : BaseFragment<FragmentPostListBinding>() {
                 else -> setCategory(AnimalType.WRONG_TYPE)
             }
         }
+        binding.rgAnimalCategories.check(R.id.rb_all)
         postListViewModel.isLocationVerified()
         postListViewModel.getPostList()
     }
 
     override fun observe() {
         postListViewModel.isLocationVerifiedLiveData.observe(this) {
-            NeedToVerifyLocationDialog(requireContext()) {
-                startVerifyLocationActivity()
-            }.callDialog()
+            if (it) {
+                binding.clBannerLocationVerified.visibility = View.VISIBLE
+            } else {
+                binding.clBannerLocationNotVerified.visibility = View.VISIBLE
+                NeedToVerifyLocationDialog(requireContext()) {
+                    startVerifyLocationActivity()
+                }.callDialog()
+            }
         }
         postListViewModel.postListLiveDate.observe(this) {
             postListAdapter.setList(it)
+            allCategory()
         }
         postListViewModel.retryEvent.observe(this) {
             makeToast(getString(R.string.try_it_later))
@@ -64,6 +76,10 @@ class PostListFragment : BaseFragment<FragmentPostListBinding>() {
         postListViewModel.unknownErrorEvent.observe(this) {
             makeToast(getString(R.string.unknown_error))
         }
+    }
+
+    private fun allCategory() {
+        isListEmpty(!postListAdapter.resetCategory())
     }
 
     private fun setCategory(category: AnimalType) {
@@ -84,5 +100,4 @@ class PostListFragment : BaseFragment<FragmentPostListBinding>() {
         val intent = Intent(context, CoordinateActivity::class.java)
         startActivity(intent)
     }
-
 }
