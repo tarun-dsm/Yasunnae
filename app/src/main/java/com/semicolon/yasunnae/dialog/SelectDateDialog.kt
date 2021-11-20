d55d1a3663786a2f44bee4f6b13658d451ee97b7
shw0471 <shw0471@naver.com>
2021-11-20 23:43:02 +0900

    fix SelectDateDialog class

--- app/src/main/java/com/semicolon/yasunnae/dialog/SelectDateDialog.kt
+++ app/src/main/java/com/semicolon/yasunnae/dialog/SelectDateDialog.kt
@@ -9,13 +9,14 @@ import android.view.Window
 import androidx.databinding.DataBindingUtil
 import com.semicolon.yasunnae.R
 import com.semicolon.yasunnae.databinding.DialogSelectDateBinding
+import com.semicolon.yasunnae.util.format
 import java.util.*
 
 class SelectDateDialog(
     private val context: Context,
     private val minDate: Date? = null,
     private val maxDate: Date? = null,
-    private val onOkClick: (selectedDate: Date) -> Unit
+    private val onOkClick: (selectedDate: String) -> Unit
 ) {
 
     fun callDialog() {
@@ -32,10 +33,17 @@ class SelectDateDialog(
         dialog.setContentView(binding.root)
 
         dialog.setContentView(binding.root)
+        binding.cvSelectDate.minDate = Date().time
         if (minDate != null) binding.cvSelectDate.minDate = minDate.time
         if (maxDate != null) binding.cvSelectDate.maxDate = maxDate.time
+
+        val dateFormat = "yyyy-MM-dd"
+        var selectedDate: String = minDate?.format(dateFormat) ?: Date().format(dateFormat)
+        binding.cvSelectDate.setOnDateChangeListener { _, year, month, dayOfMonth ->
+            selectedDate = "${year}-${month + 1}-${dayOfMonth}"
+        }
         binding.btnOkSelectDate.setOnClickListener {
-            onOkClick(Date(binding.cvSelectDate.date))
+            onOkClick(selectedDate)
             dialog.dismiss()
         }
         dialog.show()
