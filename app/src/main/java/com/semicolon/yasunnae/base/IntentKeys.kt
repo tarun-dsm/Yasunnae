afea39425e55b654d2c1518ca413a91bb548a78f
nayeon <nayeonvita@naver.com>
2021-11-22 11:44:04 +0900

    update IntentKeys
    review comment key 추가

---
+++ app/src/main/java/com/semicolon/yasunnae/base/IntentKeys.kt
@@ -0,0 +1,20 @@
+package com.semicolon.yasunnae.base
+
+object IntentKeys {
+    const val KEY_POST_ID = "KeyPostId"
+    const val KEY_START_DATE = "KeyStartDate"
+    const val KEY_END_DATE = "KeyEndDate"
+    const val KEY_IS_EDIT_MODE = "KeyIsEditMode"
+    const val KEY_EDIT_POST_ID = "KeyEditPostId"
+    const val KEY_POST_CATEGORY = "KeyPostCategory"
+    const val KEY_POST_TITLE = "KeyPostTitle"
+    const val KEY_DEADLINE = "KeyDeadline"
+    const val KEY_DESCRIPTION = "KeyDescription"
+    const val KEY_CONTACTS = "KeyContacts"
+    const val KEY_POST_IMAGE_LIST = "KeyPostImageList"
+    const val KEY_PET_NAME = "KeyPetName"
+    const val KEY_PET_SPECIES = "KeyPetSpecies"
+    const val KEY_PET_SEX = "KeyPetSex"
+
+    const val KEY_COMMENT_POST = "keycommentpost"
+}
