ce14c7296972e35b641fed3a121287831a692316
shw0471 <shw0471@naver.com>
2021-11-30 00:39:35 +0900

    update ProfilePostAdapter class

---
+++ app/src/main/java/com/semicolon/yasunnae/adapter/ProfilePostsAdapter.kt
@@ -0,0 +1,40 @@
+package com.semicolon.yasunnae.adapter
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.recyclerview.widget.RecyclerView
+import com.semicolon.domain.entity.ProfilePostEntity
+import com.semicolon.yasunnae.R
+import com.semicolon.yasunnae.databinding.ItemProfilePostBinding
+
+class ProfilePostsAdapter(
+    val onPostClick: () -> Unit
+) : RecyclerView.Adapter<ProfilePostsAdapter.ViewHolder>() {
+
+    private val profilePosts = ArrayList<ProfilePostEntity>()
+
+    override fun onCreateViewHolder(
+        parent: ViewGroup,
+        viewType: Int
+    ): ProfilePostsAdapter.ViewHolder {
+        val inflater = LayoutInflater.from(parent.context)
+        return ViewHolder(inflater.inflate(R.layout.item_profile_post, parent, false))
+    }
+
+    override fun onBindViewHolder(holder: ProfilePostsAdapter.ViewHolder, position: Int) {
+        holder.binding.profilePost = profilePosts[position]
+    }
+
+    override fun getItemCount(): Int =
+        profilePosts.size
+
+    inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val binding: ItemProfilePostBinding = DataBindingUtil.bind(itemView)!!
+
+        init {
+            itemView.setOnClickListener { onPostClick() }
+        }
+    }
+}
