222e8339093794eb39021296d029027dbcb60ed5
shw0471 <shw0471@naver.com>
2021-12-01 12:55:20 +0900

    fix MainActivity class

--- app/src/main/java/com/semicolon/yasunnae/ui/main/MainActivity.kt
+++ app/src/main/java/com/semicolon/yasunnae/ui/main/MainActivity.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import com.semicolon.yasunnae.R
 import com.semicolon.yasunnae.base.BaseActivity
 import com.semicolon.yasunnae.databinding.ActivityMainBinding
+import com.semicolon.yasunnae.ui.applications.MyApplicationsListFragment
 import com.semicolon.yasunnae.ui.postlist.PostListFragment
 import com.semicolon.yasunnae.ui.profile.ProfileFragment
 import com.semicolon.yasunnae.ui.writepost.WritePostActivity
@@ -26,7 +27,7 @@ class MainActivity : BaseActivity<ActivityMainBinding>() {
             when (it.itemId) {
                 R.id.menu_home -> setFragment(PostListFragment())
                 R.id.menu_write_post -> startWritePostActivity()
-                R.id.menu_my_applications -> TODO("내 신청목록 Fragment 실행")
+                R.id.menu_my_applications -> setFragment(MyApplicationsListFragment())
                 R.id.menu_my_profile -> {
                     val profileFragment = ProfileFragment()
                     setFragment(profileFragment)
