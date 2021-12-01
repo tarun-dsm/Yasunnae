2f8e98ac36dc00702bfbc569c57375d08df93a0d
shw0471 <shw0471@naver.com>
2021-12-01 23:18:13 +0900

    fix ProfileFragment class

--- app/src/main/java/com/semicolon/yasunnae/ui/profile/ProfileFragment.kt
+++ app/src/main/java/com/semicolon/yasunnae/ui/profile/ProfileFragment.kt
@@ -13,7 +13,7 @@ import com.semicolon.yasunnae.base.BaseFragment
 import com.semicolon.yasunnae.base.IntentKeys.KEY_COMMENT_POST
 import com.semicolon.yasunnae.base.IntentKeys.KEY_IS_EDIT_MODE
 import com.semicolon.yasunnae.base.IntentKeys.KEY_POST_ID
-import com.semicolon.yasunnae.base.IntentKeys.KEY_USER_ID
+import com.semicolon.yasunnae.base.IntentKeys.KEY_REVIEW_ID
 import com.semicolon.yasunnae.databinding.FragmentProfileBinding
 import com.semicolon.yasunnae.dialog.AskDialog
 import com.semicolon.yasunnae.dialog.ReportDialog
@@ -34,7 +34,7 @@ class ProfileFragment : BaseFragment<FragmentProfileBinding>() {
         onEditClick = {
             val intent = Intent(context, WriteReviewActivity::class.java)
             intent.putExtra(KEY_IS_EDIT_MODE, true)
-            intent.putExtra(KEY_USER_ID, it.id)
+            intent.putExtra(KEY_REVIEW_ID, it.id)
             intent.putExtra(KEY_COMMENT_POST, it.comment)
             startActivity(intent)
         }, onDeleteClick = {
